20c3a8b8f87766db1167daa6453a4b8d90e4745e
Toan Slan <toanslan@PhucVM.local>
2013-11-26 11:04:42 +0700

    Optimized remove property row in ProtocolCell

--- FirstDelegateProtocolApp/ProtocolViewController.m
+++ FirstDelegateProtocolApp/ProtocolViewController.m
@@ -12,7 +12,6 @@
 @interface ProtocolViewController ()
 < UITableViewDataSource,
   UITableViewDelegate,
-  UIAlertViewDelegate,
   ProtocolCell
 >
 
@@ -32,11 +31,8 @@
 - (NSMutableArray *)getArrayFromJSON:(NSString *)keyJSON{
     
     NSMutableArray *resultArray;
-    NSString *urlString = [NSString stringWithFormat:@"http://jkshop-staging.ap01.aws.af.cm/?json=products/tk_get_list_categories"];
-    
-    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:urlString]];
     NSError *jsonParsingError = nil;
-    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://jkshop-staging.ap01.aws.af.cm/?json=products/tk_get_list_categories"]]]
                                                          options:kNilOptions
                                                            error:&jsonParsingError];
     resultArray = [[NSMutableArray alloc] init];
@@ -62,20 +58,27 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     ProtocolCell *cell = (ProtocolCell *)[tableView dequeueReusableCellWithIdentifier:@"ProtocolCell"];
     cell.categoryLabel.text = [self.categoryNameArray objectAtIndex:indexPath.row];
-    cell.row = indexPath.row;
     cell.delegate = self;
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"You've Selected By didSelectRow" message:[NSString stringWithFormat:@"%@",[self.categoryNameArray objectAtIndex:indexPath.row]] delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"You've Selected By didSelectRow"
+                                                    message:[NSString stringWithFormat:@"%@",[self.categoryNameArray objectAtIndex:indexPath.row]]
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil, nil];
     [alert show];
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 - (void)detailButtonPressed:(UITableViewCell *)cell button:(id)sender{
     NSIndexPath *indexPath = [self.categoryTableView indexPathForCell:cell];
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"You've Selected By Delegate" message:[NSString stringWithFormat:@"%@",[self.categoryNameArray objectAtIndex:indexPath.row]] delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"You've Selected By Delegate"
+                                                    message:[NSString stringWithFormat:@"%@",[self.categoryNameArray objectAtIndex:indexPath.row]]
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil, nil];
     [alert show];
 }
 
